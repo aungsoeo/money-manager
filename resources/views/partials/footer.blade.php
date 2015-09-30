86110804d4f94f0581eb732248759ba593e92363
Savan Koradia <savan.koradia@multidots.in>
2015-09-30 13:27:37 +0530

    updated footer and layout

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,6 +6,11 @@
                 <a href="{{URL::to('/')}}">{{{trans('site/site.site_title')}}}</a>
             </span>
             <span class="hidden-phone" style="text-align: right; float: right">
-                Powered by: <a href="http://laravel.com/" alt="Laravel 5.1">Savan Koradia</a></span></p>
+                Developed by: <a href="http://savankoradia.com/" alt="Savan Koradia">Savan Koradia</a>
+            </span>
+            <span class="hidden-phone" style="text-align: right; float: right">
+                Powered by: <a href="http://laravel.com/" alt="Laravel 5.1">Laravel 5.1</a>
+            </span>
+        </p>
     </div>
 </div>
--- resources/views/site/layouts/default.blade.php
+++ resources/views/site/layouts/default.blade.php
@@ -60,17 +60,7 @@
     </div>
     <div id="push"></div>
 </div>
-<hr>
-<div id="footer">
-    <div class="container">
-        <p class="text-muted credit">
-            <span style="text-align: left; float: left">&copy; <?php echo date('Y'); ?>
-                <a href="{{URL::to('/')}}">{{{trans('site/site.site_title')}}}</a>
-            </span>
-            <span class="hidden-phone" style="text-align: right; float: right">
-                Powered by: <a href="http://laravel.com/" alt="Laravel 5.1">Savan Koradia</a></span></p>
-    </div>
-</div>
+@include('partials.footer')
 @yield('scripts')
 </body>
 </html>
